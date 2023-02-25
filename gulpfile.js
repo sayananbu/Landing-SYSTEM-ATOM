02ec0c92312203d3a5abfab5acaf49ad7b3d3aee
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-02-25 20:13:38 +0300

    28

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 
 const autoprefixer = require('gulp-autoprefixer');
-const minify = require('gulp-css-minify')
 const gulp = require('gulp');
+const minify = require('gulp-css-minify');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('prefixer',()=>{
 	return gulp.src('css/*.css')
 		.pipe(autoprefixer({
-			overrideBrowserslist:['last 3 versions']
+			overrideBrowserslist:['last 5 versions']
 		}))
 		.pipe(minify())
 		.pipe(gulp.dest('prod'))
+})
+
+gulp.task('minimages', ()=>{
+	return gulp.src('/all_photo_jpg/*.jpg')
+	.pipe(imagemin())
+	.pipe(gulp.dest('/all_photo_jpg_min'))
 })
--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 
 $("#navbar-scrollspy").scrollspy({ offset: -50 });
 
-const burger = document.querySelector('.nav-section-mobile__burger-menu')
-const navbar = document.querySelector('.nav-section-mobile__navbar')
+const burger = document.querySelector('.nav-section__burger-menu')
+const navbar = document.querySelector('.nav-section__navbar')
 const overlay = document.querySelector('.menu-overlay')
 
 burger.addEventListener('click',openMenu)
+navbar.addEventListener('click',e=>{e.stopPropagation()})
 overlay.addEventListener('click',openMenu)
+
 function openMenu(){
 	if(burger.classList.contains('menu__opened')){
 		burger.classList.remove('menu__opened')
