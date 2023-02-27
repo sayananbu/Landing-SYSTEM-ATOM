c40a6e00361a6559896e9b521b5225107e02f7d0
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-02-27 17:26:55 +0300

    31

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ import autoprefixer from 'gulp-autoprefixer'
 import minify from 'gulp-css-minify'
 import imagemin from 'gulp-imagemin'
 import minsvg from 'gulp-svgmin'
-let autoprefixBrowsers = ['> 1%', 'last 3 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
+let autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
 gulp.task('prefixer',()=>{
 	return gulp.src('css/*.css')
 		.pipe(autoprefixer({
