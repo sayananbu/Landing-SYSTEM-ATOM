b045afa8fa2d08b5b1b05527fc7b3d34a5d967b3
sayananbu <67857676+sayananbu@users.noreply.github.com>
2023-02-27 16:11:14 +0300

    30

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 
-const autoprefixer = require('gulp-autoprefixer');
-const gulp = require('gulp');
-const minify = require('gulp-css-minify');
-const imagemin = require('gulp-imagemin');
+// const autoprefixer = require('gulp-autoprefixer');
+// const gulp = require('gulp');
+// const minify = require('gulp-css-minify');
+// const imagemin = require('gulp-imagemin');
 
+import gulp from 'gulp'
+import autoprefixer from 'gulp-autoprefixer'
+import minify from 'gulp-css-minify'
+import imagemin from 'gulp-imagemin'
+import minsvg from 'gulp-svgmin'
+let autoprefixBrowsers = ['> 1%', 'last 3 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
 gulp.task('prefixer',()=>{
 	return gulp.src('css/*.css')
 		.pipe(autoprefixer({
-			overrideBrowserslist:['last 5 versions']
+			browsers:autoprefixBrowsers
 		}))
-		.pipe(minify())
+		//.pipe(minify())
 		.pipe(gulp.dest('prod'))
 })
 
 gulp.task('minimages', ()=>{
-	return gulp.src('/all_photo_jpg/*.jpg')
+	return gulp.src('all_photo_png/*.png')
 	.pipe(imagemin())
-	.pipe(gulp.dest('/all_photo_jpg_min'))
+	.pipe(gulp.dest('all_photo_png_min'))
+})
+
+gulp.task('minsvg', ()=>{
+	return gulp.src('all_photo_svg/*.svg')
+	.pipe(minsvg())
+	.pipe(gulp.dest('all_photo_svg_min'))
 })
--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
-//import scrollSpy from '@sidsbrmnn/scrollspy'
-
-//const scrollSpy = require('@sidsbrmnn/scrollspy')
-//$("#navbar-scrollspy").scrollspy({ offset: -50 });
-
-// export default scrollSpy('#navbar-scrollspy', {
-//     sectionSelector: 'section',  // Query selector to your sections
-//     targetSelector: '.navbar-block', // Query selector to your elements that will be added `active` class
-//     offset: 100               // Menu item will active before scroll to a matched section 100px
-// })
 
 const burger = document.querySelector('.nav-section__burger-menu')
 const navbar = document.querySelector('.nav-section__navbar')
 const overlay = document.querySelector('.menu-overlay')
+const logo = document.querySelector('.nav-section__logo')
 
 burger.addEventListener('click',openMenu)
 navbar.addEventListener('click',e=>{e.stopPropagation()})
@@ -22,10 +13,27 @@ function openMenu(){
 		burger.classList.remove('menu__opened')
 		navbar.classList.remove('navbar__opened')
 		overlay.classList.remove('menu-overlay__opened')
+		logo.classList.remove('white-logo')
 	}
 	else{
 		overlay.classList.add('menu-overlay__opened')
 		burger.classList.add('menu__opened')
 		navbar.classList.add('navbar__opened')
+		logo.classList.add('white-logo')
 	}
 }
+
+$(document).ready(function () {
+
+	$(window).on('load', function() {
+		$('.preloader').fadeOut().end().delay(400).fadeOut('slow');
+	  });
+
+	$('.nav-section__navbar a').bind("click", function(e) {
+        var anchor = $(this);
+			$('html, body').stop().animate({
+	            scrollTop: $(anchor.attr('href')).offset().top - 55
+	        }, 100);
+		e.preventDefault();
+		});
+});
